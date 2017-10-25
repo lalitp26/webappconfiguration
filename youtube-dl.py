9d5d730872b80eb3259a58cd3b94f77c36c5e96e
lalitp777 <lalit.patil@medipta.com>
2017-10-25 19:08:59 +0530

    Added new features and bug fixes

--- youtube-dl.py
+++ youtube-dl.py
@@ -4,6 +4,7 @@ import time
 import sys
 
 DOWNLOAD_PERCENT = 0
+CURRENT_WD = os.getcwd()
 
 class MyLogger(object):
     def debug(self, msg):
@@ -24,21 +25,6 @@ def my_hook(d):
         sys.stdout.flush()
     if d['status'] == 'finished':
         print('Done downloading, now converting ...')
-    
-def print_video_info(meta):
-    if meta:
-        print 'upload date : %s' %(meta['upload_date'])
-        print 'uploader    : %s' %(meta['uploader'])
-        print 'views       : %d' %(meta['view_count'])
-        print 'likes       : %d' %(meta['like_count'])
-        print 'dislikes    : %d' %(meta['dislike_count'])
-        print 'id          : %s' %(meta['id'])
-        print 'format      : %s' %(meta['format'])
-        print 'duration    : %s' %(meta['duration'])
-        print 'title       : %s' %(meta['title'])
-        print 'description : %s' %(meta['description'])
-    else:
-        print("No data to show")
 
 ydl_opts = {
     'format': '22',
@@ -51,16 +37,81 @@ ydl_opts = {
     'progress_hooks': [my_hook],
 }
 
+# https://youtu.be/vSFH1T3SnBY
+def print_video_info(meta):
+    if meta:
+        print 'upload date : %s' %(meta['upload_date'])
+        print 'uploader    : %s' %(meta['uploader'])
+        print 'views       : %d' %(meta['view_count'])
+        print 'likes       : %d' %(meta['like_count'])
+        print 'dislikes    : %d' %(meta['dislike_count'])
+        print 'id          : %s' %(meta['id'])
+        print 'format      : %s' %(meta['format'])
+        print 'duration    : %s' %(meta['duration'])
+        print 'title       : %s' %(meta['title'])
+        fmt = meta['formats']
+        for f in fmt:
+            try:
+                #.split('-')[1]
+                print(f['format']+" "+str(f['filesize']/(1048576)) +"Mb ")
+            except:
+                pass
+        
+        print 'description : %s' %(meta['thumbnail'])
+    else:
+        print("No data to show")
+
+def create_folder(folder_name):
+    folder_path = os.path.join(os.sep, CURRENT_WD, folder_name)
+    if not os.path.exists(folder_name):
+        os.mkdir(folder_path)
+        return (True, folder_path)
+    else:
+        print("Folder already exixts")
+        folder_name = raw_input("Enter folder name manually:- ")
+        folder_path = os.path.join(os.sep, CURRENT_WD, folder_name)
+        os.mkdir(folder_path)
+        return (True, folder_path)
+
+
+def get_video_info(url):
+    try:
+        with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+            meta = ydl.extract_info(url, download=False)
+            return meta
+    except:
+        return None
+        # print_video_info(meta)
+ 
+
+def get_video_formats(meta):
+    fmt = meta['formats']
+    for f in fmt:
+        try:
+            #.split('-')[1]
+            # print(list(f['format'].split('-')[1]+" "+f['ext']+" "+str(f['filesize']/(1048576)) +"Mb "))
+            print(f['format'].split('-')[1]+" "+f['ext']+" "+str(f['filesize']/(1048576)) +"Mb ")
+        except:
+            pass
+
+def download(url):
+    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+         ydl.download([url])
+
+
 url = raw_input("Enter URL :- ")
 
 if url:
-    
-    # with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-    #     meta = ydl.extract_info(url, download=False) 
-    
-    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-        ydl.download([url])
+    information = get_video_info(url)
+    if information is not None:
+        status, path = create_folder(information['title'])
 
+        if status:
+            os.chdir(path)
+            download(url)
+        else:
+            print("Error in download please try again")
 else:
     print("*************** No URL found ***************")
+
 
